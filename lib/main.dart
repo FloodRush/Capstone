4a779d33ac59854970e0b03e869f4831db643e29
Martin Lazo <martinlazo@my.unt.edu>
2025-04-08 19:46:32 -0500

    added firebase to the code. it was a long process with lots of downloads so idk if everyone will need to download so procced at your own risk

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project/firebase_options.dart';
 import 'pages/main_navigation.dart';
 import 'package:project/theme.dart';
 import 'pages/startUp_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
