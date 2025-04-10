ccfa8015dc9a1a690b3e1ec9ffb89d169b0f855d
Martin Lazo <martinlazo@my.unt.edu>
2025-04-09 23:10:24 -0500

    started working on the profile page

---
+++ lib/components/text_box.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:project/theme.dart';
+
+class MyTextBox extends StatelessWidget {
+  final String text;
+  final String sectionName;
+  final void Function()? onPressed;
+  const MyTextBox({
+    super.key,
+    required this.text,
+    required this.sectionName,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.lightPink,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: const EdgeInsets.only(
+        left: 15,
+        bottom: 15,
+      ),
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                sectionName,
+                style: TextStyle(color: Colors.black),
+              ),
+
+              //edit button
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.settings),
+              ),
+            ],
+          ),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../components/text_box.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
+
+  Future<void> editField(String field) async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile')),
-      body: Center(child: Text('Welcome to the Profile Page')),
-    );
+        appBar: AppBar(title: Text('Welcome to the Profile Page')),
+        body: ListView(
+          children: [
+            SizedBox(height: 50),
+            Icon(
+              Icons.person,
+              size: 72,
+            ),
+            Text(
+              currentUser.email!,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Text(
+                'My details',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+            MyTextBox(
+              text: 'Test Subject',
+              sectionName: 'username',
+              onPressed: () => editField('username'),
+            ),
+            MyTextBox(
+              text: 'Empty Bio',
+              sectionName: 'bio',
+              onPressed: () => editField('bio'),
+            ),
+            SizedBox(height: 50),
+          ],
+        ));
   }
 }
