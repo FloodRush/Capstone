24d268de5a8aed8c76f7fc04e29b9ddf99876eae
iew0006 <irelynwhalen@my.unt.edu>
2025-04-12 14:31:14 -0500

    Update journal.dart
    -Fixed some issues with delete, view, and update
    -Added entry
    -Improved visuals
    -Changed text boxes

--- journal.dart
+++ journal.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Main()));//used to run flutter
+  runApp(const MaterialApp(home: Main()));
 }
 
 class Main extends StatefulWidget {
@@ -16,53 +16,66 @@ class Main extends StatefulWidget {
 }
 
 class _UIState extends State<Main> {
-  //variables
   final List<String> entry = [];
+  final List<String> name = [];
   final formkey = GlobalKey<FormState>();
   final myController = TextEditingController();
   DateTime startDate = DateTime(2025, 5, 7);
-//add function to make new journal entries
+
   void add() {
-    setState(() {entry.add(myController.text.trim());
+    setState(() {
+entry.add(myController.text.trim());
     });
   }
-//delete function to delete journal entries
-  void delete() {
+
+  delete(name) {
     setState(() {
       if (entry.isNotEmpty) {
-        entry.removeLast();
+        entry.remove(name);
       }
-    });
-  }
-//update funtion to edit journal entries
-  void update() {
+    });}
+
+   update(name) {
     setState(() {
       if (entry.isNotEmpty) {
+        ///date.insert(0, myController.text.trim());
+        name.insert(0, myController.text.trim());
         entry.insert(0, myController.text.trim());
       }
     });
   }
 //view function to view journal entries
-  void view() {
-    print(entry); //for testing
+   view(name) {
+   if (entry.isNotEmpty) {
+   // SizedBox(
+    //  height: 100,
+     // width: 30,
+     // child: Text(entry),
+    //);
+    print(entry);
   }
+   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Journal")),
-      body: Row(
+      appBar: AppBar(
+        title: Text("Journal"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.pink[200],
+      body: 
+      Row( 
         children: [
           Align(
             alignment: Alignment.centerLeft,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [ 
-                //buttons
+              children: [
                 ElevatedButton(onPressed: add, child: Text('ADD')),
-                ElevatedButton(onPressed: delete, child: Text('DELETE')),
-                ElevatedButton(onPressed: update, child: Text('UPDATE')),
-                ElevatedButton(onPressed: view, child: Text('VIEW')),
+                ElevatedButton(onPressed: () => delete(name), child: Text('DELETE')),
+                ElevatedButton(onPressed: () => update(name), child: Text('UPDATE')),
+                ElevatedButton(onPressed: () => view(name), child: Text('VIEW')),
               ],
             ),
           ),
@@ -84,7 +97,6 @@ class _UIState extends State<Main> {
                       ),
                     ),
                   ),
-                    //to aid the date entry  
                   ElevatedButton(
                     onPressed: () {
                       showCupertinoModalPopup(
@@ -104,22 +116,40 @@ class _UIState extends State<Main> {
                     },
                     child: Text('Pick Date'),
                   ),
-                  Form(
-                    key: formkey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {//checks for data
-                              return 'Enter something';
+                  Text('Name'),
+                  SizedBox(
+                    height: 30,
+                    width: 200,
+                    child: TextField(
+                      controller: myController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Input',
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                      ),
+                    ),
+                  ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (formkey.currentState!.validate()) {
+                              print("Stored!");
                             }
-                            return null;
                           },
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Name',
-                          ),
-                        ),
+                          child: Text("Confirm"),
+                        ),                 
+                  Text('Entry'),
+                  SizedBox(
+                    height: 100,
+                    width: 200,
+                    child: TextField(
+                      controller: myController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'How are you feeling?',
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                      ),
+                    ),
+                  ),
                         ElevatedButton(
                           onPressed: () {
                             if (formkey.currentState!.validate()) {
@@ -128,9 +158,6 @@ class _UIState extends State<Main> {
                           },
                           child: Text("Confirm"),
                         ),
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ),
