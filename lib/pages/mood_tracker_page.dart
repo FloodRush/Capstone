05aa38591e59940e49dfaa6b7d861136ea32bc0c
FloodRush <69131251+FloodRush@users.noreply.github.com>
2025-04-12 23:33:31 -0500

    Update mood_tracker_page.dart
    I forgot an 'i' in import smh

--- lib/pages/mood_tracker_page.dart
+++ lib/pages/mood_tracker_page.dart
@@ -1,4 +1,4 @@
-mport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MoodTrackerPage extends StatefulWidget {
