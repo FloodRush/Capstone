bdbf8badc40678680b43ab796faac2c74e7dc73d
Thi Van Anh Nguyen <AnhThiVanNguyen@my.unt.edu>
2025-08-09 23:08:48 -0500

    update color and homepageUI

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,9 +2,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color darkPink = Color.fromARGB(255, 231, 125, 160);
-  static const Color hotPink = Color.fromARGB(255, 248, 87, 140);
-  static const Color lightPink = Color.fromARGB(255, 247, 199, 215);
+  static const Color darkPink = Color.fromARGB(255, 248, 159, 187);
+  static const Color hotPink = Color.fromARGB(255, 246, 175, 181);
+  static const Color lightPink = Color.fromARGB(255, 248, 220, 229);
 }
 
 LinearGradient appGradientBackground() {
