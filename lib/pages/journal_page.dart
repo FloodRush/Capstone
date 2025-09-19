a4afd235673ff485b4dd2fd8c726c589d57eb084
iew0006 <irelynwhalen@my.unt.edu>
2025-09-19 09:51:55 -0500

    Update journal_page.dart
    Added database storing to the functions

--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import '../pages/Create_Account.dart';
+//NOTE: ADD THE COMMENTS BACK
 class JournalPage extends StatefulWidget {
   const JournalPage({super.key});
 
@@ -17,18 +18,6 @@ class JournalPage extends StatefulWidget {
 //retrieving ids
 //Stream<QuerySnapshot> getUser()
 //{
-  var user = FirebaseAuth.instance;
-
-  //if(user != null)
-  //{
-    //return FirebaseFirestore.instance
-     // .collection("Users")
-     // .where('id', isEqualTo: user.myId)
-      //.snapshots();
-    //}
-     // else{print("This user does not exist");}
-//}
-
 class _UIState extends State<JournalPage> {
   final List<String> entry = [];
   final List<String> name = [];
@@ -37,7 +26,19 @@ class _UIState extends State<JournalPage> {
   final dateController = TextEditingController();
   final nameController = TextEditingController();
   final entryController = TextEditingController();
+  final deleteController = TextEditingController();
   DateTime startDate = DateTime(2025, 5, 7);
+
+  final userInstance = FirebaseAuth.instance;
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    if (user == null) {
+      print("This user does not exist");
+    }
+  }
 
   void add() {
     setState(() {
@@ -45,10 +46,46 @@ class _UIState extends State<JournalPage> {
       name.add(nameController.text.trim());
       entry.add(entryController.text.trim());
     });
+
+    FirebaseFirestore.instance
+        .collection("Entries")
+        .doc(userInstance.currentUser!.uid)
+        .set({
+      'date': dateController.text.trim(),
+      'name': nameController.text.trim(),
+      'entry': entryController.text.trim(),
+      'uid': userInstance.currentUser!.uid
+    });
   }
 
-  delete(deleted) {
+  void delete(String deleted) {
     int i = name.indexOf(deleted.trim());
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        if (entry.isNotEmpty && i != -1) {
+          return AlertDialog(
+            title: Text('Select Entry'),
+            content: SizedBox(
+              height: 30,
+              width: 200,
+              child: TextField(
+                controller: deleteController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return SizedBox.shrink();
+        }
+      },
+    );
+
     setState(() {
       if (entry.isNotEmpty && i != -1) {
         name.removeAt(i);
@@ -60,7 +97,7 @@ class _UIState extends State<JournalPage> {
     });
   }
 
-  update(updated) {
+  void update(String updated) {
     setState(() {
       int i = name.indexOf(updated.trim());
       if (entry.isNotEmpty && i != -1) {
@@ -72,35 +109,33 @@ class _UIState extends State<JournalPage> {
       }
     });
   }
-
 //view function to view journal entries
-  view(viewed) {
+  void view(String viewed) {
     int i = name.indexOf(viewed.trim());
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          if (entry.isNotEmpty && i != -1) {
-            return AlertDialog(
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        if (entry.isNotEmpty && i != -1) {
+          return AlertDialog(
             title: Text('Name: ${name[i]}'),
             content: Text('Date: ${date[i]}\n\n${entry[i]}'),
-              );
-            } else if (entry.isEmpty) {
-        print('Entry does not exist');
-        return AlertDialog(
-          title: Text('Error'),
-          content: Text('Entry does not exist'),
-        );
+          );
+        } else if (entry.isEmpty) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Entry does not exist'),
+          );
         } else {
-        return AlertDialog(
-          title: Text('Invalid'),
-          content: Text('Invalid index'),
-        );
-      }
-    },
-  );
-}
+          return AlertDialog(
+            title: Text('Invalid'),
+            content: Text('Invalid index'),
+          );
+        }
+      },
+    );
+  }
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -117,37 +152,27 @@ class _UIState extends State<JournalPage> {
               children: [
                 ElevatedButton(onPressed: add, child: Text('ADD')),
                 ElevatedButton(
-                    onPressed: () => delete(nameController.text),
-                    child: Text('DELETE')),
+                  onPressed: () => delete(nameController.text),
+                  child: Text('DELETE'),
+                ),
                 ElevatedButton(
-                    onPressed: () => update(nameController.text),
-                    child: Text('UPDATE')),
+                  onPressed: () => update(nameController.text),
+                  child: Text('UPDATE'),
+                ),
                 ElevatedButton(
-                    onPressed: () => view(nameController.text),
-                    child: Text('VIEW')),
+                  onPressed: () => view(nameController.text),
+                  child: Text('VIEW'),
+                ),
               ],
             ),
           ),
           Expanded(
             child: Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('Date'),
-                  SizedBox(
-                    height: 30,
-                    width: 200,
-                    child: TextField(
-                      controller: dateController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Input',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
+                  GestureDetector(
+                    onTap: () {
                       showCupertinoModalPopup(
                         context: context,
                         builder: (context) => SizedBox(
@@ -155,7 +180,11 @@ class _UIState extends State<JournalPage> {
                           child: CupertinoDatePicker(
                             initialDateTime: startDate,
                             onDateTimeChanged: (DateTime date) {
-                              setState(() => startDate = date);
+                              setState(() {
+                                startDate = date;
+                                dateController.text =
+                                    "${date.year}/${date.month}/${date.day}";
+                              });
                             },
                             use24hFormat: true,
                             mode: CupertinoDatePickerMode.date,
@@ -163,8 +192,26 @@ class _UIState extends State<JournalPage> {
                         ),
                       );
                     },
-                    child: Text('Pick Date'),
+                    child: Column(
+                      children: [
+                        Text('Date'),
+                        SizedBox(
+                          height: 30,
+                          width: 200,
+                          child: TextField(
+                            controller: dateController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Input',
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 10.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  SizedBox(height: 20),
                   Text('Name'),
                   SizedBox(
                     height: 30,
@@ -174,11 +221,12 @@ class _UIState extends State<JournalPage> {
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Name',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10.0),
                       ),
                     ),
                   ),
-                  
+                  SizedBox(height: 20),
                   Text('Entry'),
                   SizedBox(
                     height: 100,
@@ -188,7 +236,8 @@ class _UIState extends State<JournalPage> {
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'How are you feeling?',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10.0),
                       ),
                     ),
                   ),
